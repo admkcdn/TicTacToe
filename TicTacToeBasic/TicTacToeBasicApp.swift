f908d501efc98ddfabbab9675ffd01916b4035f7
Adem Kocdogan <adem.kocdogan@icloud.com>
2024-03-26 15:44:19 +0300

    Initial Commit

---
+++ TicTacToeBasic/TicTacToeBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeBasicApp.swift
+//  TicTacToeBasic
+//
+//  Created by Adem Koçdoğan on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
